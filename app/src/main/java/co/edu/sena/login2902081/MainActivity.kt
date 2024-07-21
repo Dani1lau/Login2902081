407efa4baae3187e7765d1fcc996a2a0b53c1f20
Dani1lau <soydanielra16@gmail.com>
2024-07-21 17:44:05 -0500

    commit del main

--- app/src/main/java/co/edu/sena/login2902081/MainActivity.kt
+++ app/src/main/java/co/edu/sena/login2902081/MainActivity.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import co.edu.sena.login2902081.screen.SingUpScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            SingUpScreen()
         } //setContent
     } //onCreate
 } //MainActivity
 
-//@preview
+@Preview(showBackground = true)
+@Composable
+fun MainActivityPreview() {
+    MainActivity()
+}
